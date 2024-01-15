55f4482b116cfe71a6556de0496f9de09cce0e31
azizul96 <alifalamin42@gmail.com>
2024-01-16 03:24:31 +0600

    Page added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,11 @@
 "use client"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 const Navbar = () => {
     const [open, setOpen] = useState(false)
+    const pathname = usePathname()
     const routes = [
         {id: 1, path: "/", name: "Home"},
         {id: 2, path: "/about", name: "About"},
@@ -23,9 +25,9 @@ const Navbar = () => {
                         : <i class="fa-solid fa-bars"></i>
                     }
                 </div>
-                <ul className={`md:flex gap-10 ${open ? "" : "hidden"}`}>
+                <ul className={`md:flex items-center gap-10  ${open ? "" : "hidden"}`}>
                     {
-                        routes.map(route => <li className="" key={route.id}><Link href={route.path}>{route.name}</Link></li>)
+                        routes.map(route => <li className="" key={route.id}><Link className={`link ${pathname === route.path ? 'underline ' : ''}`} href={route.path}>{route.name}</Link></li>)
                     }
                 </ul>
                 
